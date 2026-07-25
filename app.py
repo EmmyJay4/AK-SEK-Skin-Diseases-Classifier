1b3b5ac9bfe2a6dded28e86e65dc4c6463b105c3
EmmyJay4 <emmysco44@gmail.com>
2026-07-26 00:40:09 +0100

    Fix typo in page title for skin lesion classifier

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import tensorflow as tf
 from PIL import Image
 import pathlib
 
-st.set_page_config(page_title="AK vs SEK Skin Lesion Classifier", page_icon="🩺", layout="centered")
+st.set_page_config(page_title="AK vs SEK Skin Lesson Classifier", page_icon="🩺", layout="centered")
